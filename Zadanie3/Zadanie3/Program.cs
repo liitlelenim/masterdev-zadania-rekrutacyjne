dd698ce59313b553a68f26720577642e342bd11e
liitlelenim <jakubblaszczyk20@gmail.com>
2022-06-06 21:35:10 +0200

    add zadanie3 directory

---
+++ Zadanie3/Zadanie3/Program.cs
@@ -0,0 +1,18 @@
+﻿string fileDirectoryPath = "./";
+string fileName = "plik-testowy";
+string extension = ".txt";
+
+StreamReader reader = new StreamReader(fileDirectoryPath + fileName + extension);
+string newFileContent = reader
+    .ReadToEnd()
+    .Replace("praca", "job");
+reader.Close();
+
+File.Delete(fileDirectoryPath + fileName + extension);
+DateTime date = DateTime.Now;
+
+StreamWriter writer =
+    new StreamWriter(fileDirectoryPath + fileName + "_changed" + date.ToString().Replace(".", "")[0..8] + extension);
+
+writer.Write(newFileContent);
+writer.Close();
